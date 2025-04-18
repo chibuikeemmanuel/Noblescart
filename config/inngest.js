2652e09049f968bd71c9294b05a058985b3353eb
Your Name <you@example.com>
2025-04-18 14:42:58 +0100

    new commit

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDb from "@/lib/connectDb";
-import User from "@/models/User";
+import User from '../../models/User'; // adjust based on relative path from inngest.js
+
 
 // ✅ CREATE CLIENT WITH ID
 export const inngest = new Inngest({ id: "quickcart-app" });
