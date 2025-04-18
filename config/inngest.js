0b9c5d6a62f04b3908493df298f1e997597feb7a
Your Name <you@example.com>
2025-04-18 14:48:08 +0100

    purrr

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDb from "@/lib/connectDb";
-import User from '../../models/User'; // adjust based on relative path from inngest.js
+import User from '../models/User';
+ // adjust based on relative path from inngest.js
 
 
 // ✅ CREATE CLIENT WITH ID
