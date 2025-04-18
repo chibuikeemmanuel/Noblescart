c99ae28fcaeb4dded49233c0cb114f50d76f04a4
Your Name <you@example.com>
2025-04-18 16:14:25 +0100

    now

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDb from "@/lib/connectDb";
-import User from '../models/User';
+import User from "@/models/User";
+
  // adjust based on relative path from inngest.js
 
 
