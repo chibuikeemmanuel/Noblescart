d4d0667a363a614b661efdf4a4c6a6d75b773837
Your Name <you@example.com>
2025-04-18 09:56:43 +0100

    first commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,13 @@
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
 
+
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
-    syncUserUpdate, // ✅ Correct function name
+    syncUserUpdate, // ✅ this should match the actual function name
     syncUserDeletion,
-  ],
+  ]
+  ,
 });
